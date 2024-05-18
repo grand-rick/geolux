534573361c144bc83a6250485e43f58b769b760f
Patrick Murimi <kabugap02@gmail.com>
2024-05-18 15:21:48 +0300

    fix: lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,49 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './main/home/home.component';
+import { authGuard } from './guards/auth.guard';
+import { LocationComponent } from './main/location/location.component';
 
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
-  },
-  {
-    path: '**',
-    redirectTo: '',
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./login/login.component').then((m) => m.LoginComponent),
+      },
+      {
+        path: 'location',
+        canActivate: [authGuard],
+        component: LocationComponent,
+      },
+      {
+        path: 'contact-us',
+        loadComponent: () =>
+          import('./main/contact-us/contact-us.component').then(
+            (m) => m.ContactUsComponent
+          ),
+      },
+      {
+        path: 'place/:region_id',
+        loadComponent: () =>
+          import('./main/places-region/places-region.component').then(
+            (m) => m.PlacesRegionComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: 'home',
+      },
+    ],
   },
 ];
